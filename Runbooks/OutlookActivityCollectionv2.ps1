41bce7f894d438c7ec85db910725feab26630fa2
Bryan Zabchuk <bzabber@gmail.com>
2023-03-24 14:31:10 -0400

    Add Log Analytics ingestion post.

--- Runbooks/OutlookActivityCollectionv2.ps1
+++ Runbooks/OutlookActivityCollectionv2.ps1
@@ -48,13 +48,59 @@ $tenants = @(
     }#>
 )
 
-#param (
-#    $TenantId,
-#    $ClientId,
-#    $ClientSecret
-#)
+#Log Analytics components
+$LogAnalyticsWorkspaceID = "6c103a25-8ac4-4421-8a51-03b4c8082eb7" #Get-AzAutomationVariable -Name 'LAWWorkSpaceID'
+$SharedKey = "AhLfQV/YgsSHXEafWJOMuSDTeWRZ2+++ErLzxJ65PBITRme/0Gh8Mk45t/f/hnOY+bvUdwbshzpGOh/S0EBcuA==" #Get-AzAutomationVariable -Name 'LAWPrimaryKey'
+$LogType = "OutlookActivityCollection"
 
+# Function to to create the authorization signature
+Function Build-Signature($customerId, $sharedKey, $date, $contentLength, $method, $contentType, $resource)
+    {
+        $xHeaders = "x-ms-date:" + $date
+        $stringToHash = $method + "`n" + $contentLength + "`n" + $contentType + "`n" + $xHeaders + "`n" + $resource
 
+        $bytesToHash = [Text.Encoding]::UTF8.GetBytes($stringToHash)
+        $keyBytes = [Convert]::FromBase64String($sharedKey)
+
+        $sha256 = New-Object System.Security.Cryptography.HMACSHA256
+        $sha256.Key = $keyBytes
+        $calculatedHash = $sha256.ComputeHash($bytesToHash)
+        $encodedHash = [Convert]::ToBase64String($calculatedHash)
+        $authorization = 'SharedKey {0}:{1}' -f $customerId, $encodedHash
+        return $authorization
+    }
+
+# Function to Post data to Azure Monitor Log Analytics Data Collection Endpoint
+# Create the function to create and post the request
+Function Post-LogAnalyticsData($customerId, $sharedKey, $body, $logType) {
+    $method = "POST"
+    $contentType = "application/json"
+    $resource = "/api/logs"
+    $rfc1123date = [DateTime]::UtcNow.ToString("r")
+    $contentLength = $body.Length
+    $signature = Build-Signature `
+        -customerId $customerId `
+        -sharedKey $sharedKey `
+        -date $rfc1123date `
+        -contentLength $contentLength `
+        -method $method `
+        -contentType $contentType `
+        -resource $resource
+    $uri = "https://" + $customerId + ".ods.opinsights.azure.com" + $resource + "?api-version=2016-04-01"
+
+    $headers = @{
+        "Authorization"        = $signature;
+        "Log-Type"             = $logType;
+        "x-ms-date"            = $rfc1123date;
+        "time-generated-field" = $TimeStampField;
+    }
+
+    $response = Invoke-WebRequest -Uri $uri -Method $method -ContentType $contentType -Headers $headers -Body $body -UseBasicParsing
+    return $response.StatusCode
+
+}
+
+# Connect to each tenant and collect Outlook Activity.
 foreach ($tenant in $tenants) {
     $tenantName = $tenant.TenantName
     $tenantID = $tenant.TenantID
@@ -132,5 +178,5 @@ foreach ($tenant in $tenants) {
     Write-Output "Boo"
     #Export result to CSV
     #Write-Host $resultarray
-    #$resultarray | Export-Csv "C:\temp\EmailActivityCount.csv" -NoTypeInformation
+    Post-LogAnalyticsData -customerId $customerId -sharedKey $sharedKey -body ([System.Text.Encoding]::UTF8.GetBytes($json)) -logType $logType
 }
