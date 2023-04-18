17258ae1b1eb077c99120bdc8c653fe09f920d9b
Bryan Zabchuk <bzabber@gmail.com>
2023-04-18 15:42:23 -0400

    Adding v3 version of the powershell based runbook.

---
+++ Runbooks/OutlookActivityCollectionv3.ps1
@@ -0,0 +1,185 @@
+<#PSScriptInfo
+.DESCRIPTION Query the Graph Report Endpoint:- Get the total number of user mailboxes in your organization and how many are active each day of the reporting period.
+https://docs.microsoft.com/en-us/graph/api/reportroot-getmailboxusagemailboxcounts?view=graph-rest-1.0
+
+Prerequisite https://docs.microsoft.com/en-us/graph/auth-register-app-v2?context=graph%2Fapi%2F1.0&view=graph-rest-1.0
+Below application permission is required:
+Reports.Read.All
+
+#NOTE - Disclaimer
+#Following programming examples is for illustration only, without warranty either expressed or implied,
+#including, but not limited to, the implied warranties of merchantability and/or fitness for a particular purpose. 
+#This sample code assumes that you are familiar with the programming language being demonstrated and the tools 
+#used to create and debug procedures. This sample code is provided for the purpose of illustration only and is 
+#not intended to be used in a production environment. 
+
+#>
+
+<#
+.SYNOPSIS Query Office 365 status
+
+.PARAMETER TenantID
+The Tenant ID of your Office 365 instance.
+
+.PARAMETER ClientID
+Use the App Registration ID.
+
+.PARAMETER ClientSecret
+Use the client secret you create as part of the app registration.
+
+#>
+#./Get-emailActivityUsageRpt.ps1 -TenantID 'tenantId' -ClientID 'clientId' -ClientSecret '********' | Out-File -FilePath Logoutput.txt
+#Import-Module MSAL.PS
+#--- Include module to format and send request to OMS ---#
+#Import-Module OMSIngestionAPI
+Add-Type -AssemblyName System.Web
+
+### Step 0: Set variables required for the rest of the script.
+
+# information needed to authenticate to AAD and obtain a bearer token
+$tenantId = "b9e8fd52-6d6e-4bd9-b191-c776fb921da4" #Tenant ID the data collection endpoint resides in
+$appId = "934a0104-d421-4c89-b8bb-c66ede05c932" #Application ID created and granted permissions
+$appSecret = "wGI8Q~BG_rV1UjCMekhBbLfihUQUFU3Qeq6K~aB~" #Secret created for the application
+
+# information needed to send data to the DCR endpoint
+$dceEndpoint = "https://m365datacollection-ep-h5wz.canadacentral-1.ingest.monitor.azure.com" #the endpoint property of the Data Collection Endpoint object
+$dcrImmutableId = "dcr-752fab2113b141baa82581cf7faa6064" #the immutableId property of the DCR object
+$streamName = "Custom-M365OutlookActivityCollection_CL" #name of the stream in the DCR that represents the destination table
+
+### Step 1: Obtain a bearer token used later to authenticate against the DCE.
+try {
+    Write-Output "Building token for DCE Endpoint."
+    $scope = [System.Web.HttpUtility]::UrlEncode("https://monitor.azure.com//.default")   
+    $body = "client_id=$appId&scope=$scope&client_secret=$appSecret&grant_type=client_credentials";
+    $headers = @{"Content-Type" = "application/x-www-form-urlencoded" };
+    $uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+    Write-Output "Token build SUCCESSFUL"
+}
+catch [System.Net.WebException] {
+    Write-Warning "Exception: $($_.Exception.Message)"
+    Write-Warning "FAILED creating DCE token." 
+    $ErrMsg = "$_"
+    Write-Output $ErrMsg
+}
+
+
+$bearerToken = (Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -Headers $headers).access_token
+
+$tenants = @(
+    [pscustomobject]@{
+        TenantName   = "Contoso";
+        TenantID     = Get-AutomationVariable -Name'M365x87145483TenantID';
+        ClientID     = Get-AutomationVariable -Name 'M365x87145483ClientID';
+        ClientSecret = Get-AutomationVariable -Name 'M365x87145483ClientSecret';
+    }<#,
+    [pscustomobject]@{
+        TenantName   = "CustomerName";
+        TenantID     = Get-AutomationVariable -Name 'CustomerTenantID';
+        ClientID     = Get-AutomationVariable -Name 'CustomerClientID';
+        ClientSecret = Get-AutomationVariable -Name 'CustomerClientSecret';
+    }#>
+)
+
+foreach ($tenant in $tenants) {
+    $tenantName = $tenant.TenantName
+    $tenantID = $tenant.TenantID
+    $clientID = $tenant.ClientID
+    $ClientSecret = $tenant.ClientSecret
+    
+    Write-Output " Processing ... " $tenantName
+
+
+    # OAuth Token Endpoint
+    $uri = "https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token"
+    #Write-Host $uri
+    # Construct Body for OAuth Token
+    $body = @{
+        client_id     = $clientID
+        scope         = "https://graph.microsoft.com/.default"
+        client_secret = $ClientSecret
+        grant_type    = "client_credentials"
+    }
+
+    # Get Token
+    $TokenRequest = try {
+        Invoke-RestMethod -Method Post -Uri $uri -ContentType "application/x-www-form-urlencoded" -Body $body -ErrorAction Stop
+    }
+    catch [System.Net.WebException] {	
+        Write-Warning "Exception was caught: $($_.Exception.Message)" #+ "for "$tenantName
+    }
+
+    If ($TokenRequest) { $token = $TokenRequest.access_token }
+    Else { "No token detected. Ending."; Break }
+    #write-host $token
+   
+     
+    # Create an empty array to store the result.
+    $QueryResults = @()
+    $UTCDateTime = [DateTime]::UtcNow.ToString('u')
+
+    # Invoke REST method and fetch data until there are no pages left.
+    $requestURI = "https://graph.microsoft.com/v1.0/reports/getEmailActivityCounts(period='D7')"                
+    do {
+        $Results = try {
+            Invoke-RestMethod -Method Get -Uri $requestURI -ContentType "application/json" -Headers @{ Authorization = "Bearer $token" } -ErrorAction Stop
+        }
+        catch [System.Net.WebException] {
+            Write-Warning "Exception: $($_.Exception.Message) for " $tenantName
+        }
+        if ($Results.value) {
+            
+            $QueryResults += $Results.value
+            
+                                                                    
+        }
+        else {
+            $QueryResults += $Results
+        }
+        $requestURI = $Results.'@odata.nextlink'
+                                                                                                
+    } until (!($requestURI))
+                                            
+    # Return the results
+
+    #$QueryResults
+    
+
+
+    #Remove special chars from header
+    #$QueryResults = $QueryResultsCsv
+    $QueryResults = $QueryResults.Replace('ï»¿Report Refresh Date', 'Report Refresh Date')
+
+    #$QueryResults
+
+    #Convert the stream result to an array  
+    $ResultsArray = ConvertFrom-Csv $QueryResults
+    $ResultsArray | Add-Member -NotePropertyName Tenant -NotePropertyValue $tenantName
+    $ResultsArray | Add-Member -NotePropertyName TimeGenerated -NotePropertyValue $UTCDateTime
+    #$ResultsArray
+
+    
+
+    #Convert to JSON
+    Write-Output "Converting to JSON..."
+    $JSON = ConvertTo-Json -InputObject $ResultsArray -Depth 10
+
+    #$JSON | Set-Content M365OutlookActivityCollection.json
+    #$JSON
+    #Post to Log Analytics
+    try {
+        Write-Output "Posting to Log Analytics Workspace ..."
+        $body = $JSON;
+        $headers = @{"Authorization" = "Bearer $bearerToken"; "Content-Type" = "application/json"};
+        $uri = "$dceEndpoint/dataCollectionRules/$dcrImmutableId/streams/$($streamName)?api-version=2021-11-01-preview"
+        $uploadResponse = Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -Headers $headers
+        Write-Output "Upload to Log Analytics workspace SUCCESSFUL"
+    }
+    catch [System.Net.WebException] {
+        $SendStatus = "failure"
+        Write-Warning "Exception: $($_.Exception.Message)"
+        Write-Output "FAILED digesting for $tenantName" 
+        $TransMsg = "$_"
+        Write-Output $TransMsg
+    }
+}
+
